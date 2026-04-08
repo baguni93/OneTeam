0264ae4749ad79b2d73a5f01a2f14600221fe86c
jimmy@111percent.net <jimmy@111percent.net>
2026-04-08 14:29:55 +0900

    라우터 index.js 추가

--- jangoya/src/App.vue
+++ jangoya/src/App.vue
@@ -2,8 +2,16 @@
   <div>
     <h2>콘솔을 확인합니다.</h2>
   </div>
+
+  <div>
+    <Header />
+    <router-view></router-view>
+  </div>
 </template>
+
 <script setup>
+import Header from './components/Header.vue';
+
 import axios from 'axios';
 const requestAPI = async () => {
   const url = '/api/todos';
--- jangoya/src/router/index.js
+++ jangoya/src/router/index.js
@@ -1,8 +1,95 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Main from '@/pages/Main.vue';
+import Transction from '@/pages/Transction.vue';
+import AddTransction from '@/pages/AddTransction.vue';
+import SelectCategory from '@/pages/SelectCategory.vue';
+import EditTransction from '@/pages/EditTransction.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    { path: '/user/login', name: 'user/login', component: Login },
+    { path: '/user/mypage', name: 'mypage', component: Mypage },
+    {
+      path: '/user/findaccount',
+      name: 'user/findaccount',
+      component: FindAccount,
+    },
+    { path: '/user/register', name: 'user/register', component: Register },
 
-export default router
+    { path: '/', name: 'main', component: Main },
+    { path: '/transction', name: 'transction', component: Transction },
+    {
+      path: '/transction/add',
+      name: 'transction/add',
+      component: AddTransction,
+    },
+    {
+      path: '/transaction/select/category',
+      name: 'transaction/select/category',
+      component: SelectCategory,
+    },
+    {
+      path: '/transaction/edit/:id',
+      name: 'transaction/edit/:id',
+      component: EditTransction,
+    },
+    {
+      path: '/search',
+      name: 'search',
+      component: Search,
+    },
+    {
+      path: '/category/filter',
+      name: 'category/filter',
+      component: CategoryFilter,
+    },
+    {
+      path: '/mypage/category',
+      name: 'mypage/category',
+      component: Category,
+    },
+    {
+      path: '/mypage/category/edit',
+      name: 'mypage/category/edit',
+      component: EditCategory,
+    },
+    {
+      path: '/mypage/category/add',
+      name: 'mypage/category/add',
+      component: AddCategory,
+    },
+    {
+      path: '/categoryicon',
+      name: 'categoryicon',
+      component: Categoryicon,
+    },
+    {
+      path: '/categorycolor',
+      name: 'categorycolor',
+      component: CategoryColor,
+    },
+    {
+      path: '/budget',
+      name: 'budget',
+      component: Budget,
+    },
+    {
+      path: '/budget/new',
+      name: 'budget/new',
+      component: NewBudget,
+    },
+    {
+      path: '/budget/edit/:id',
+      name: 'budget/edit/:id',
+      component: EditBudget,
+    },
+    {
+      path: '/budget/delete',
+      name: 'budget/delete',
+      component: DeleteBudget,
+    },
+  ],
+});
+
+export default router;
