ec05d889df5a59ac0d62ed6dd74a203abe33796e
윤지 <shinyunjy412@naver.com>
2026-04-09 16:27:35 +0900

    search 기능구현 완료

--- jangoya/src/App.vue
+++ jangoya/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <h2>콘솔을 확인합니다.</h2>
+    <!-- <h2>콘솔을 확인합니다.</h2> -->
   </div>
 
   <div>
     <Header />
     <router-view></router-view>
+    <Search />
   </div>
 </template>
 
 <script setup>
 import Header from './components/Header.vue';
+import Search from './pages/user/Search.vue';
 </script>
